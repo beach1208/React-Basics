339c560cd99e402a748c95cea35219ab48ae8720
Nagisa Kojima <nk.bg.1208@gmail.com>
2018-12-04 17:42:21 -0800

    Count game added

--- src/components/StatePractice.js
+++ src/components/StatePractice.js
@@ -5,10 +5,16 @@ class StatePractice extends Component {
     super(props);
 
     this.state = {
-      name: "Nagisa Kojima"
+      name: "Nagisa Kojima",
+      count: 0,
+      input: " "
     };
 
     this.handleClick = this.handleClick.bind(this);
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+    this.inputChange = this.inputChange.bind(this);
   }
 
   handleClick() {
@@ -16,6 +22,32 @@ class StatePractice extends Component {
       name: "Brandt Williams"
     });
   }
+
+  increment() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  decrement() {
+    this.setState({
+      count: this.state.count - 1
+    });
+  }
+
+  reset() {
+    if (this.state.count !== 0)
+      this.setState({
+        count: 0
+      });
+  }
+
+  inputChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+
   render() {
     return (
       <div>
@@ -23,6 +55,14 @@ class StatePractice extends Component {
           My name is : <strong>{this.state.name}</strong>
         </p>
         <button onClick={this.handleClick}>Click me!</button>
+
+        <p>Current Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decremenet</button>
+        <button onClick={this.reset}>Reset</button>
+
+        <input value={this.state.input} onChange={this.inputChange} />
+        <p>Typed from input: {this.state.input}</p>
       </div>
     );
   }
